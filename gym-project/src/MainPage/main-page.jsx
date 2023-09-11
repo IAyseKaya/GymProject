913bff01ea4a9fd9b803f871cd22ffd8ac9b2a40
IAyseKaya <i.ayse-kaya@hotmail.com>
2023-09-11 23:32:35 +0300

    form eklendi.

--- gym-project/src/MainPage/main-page.jsx
+++ gym-project/src/MainPage/main-page.jsx
@@ -133,11 +133,98 @@ function MainPage() {
                     </div>
                 </div>
             </div>
-            <div className="index">
+            <div className="index w-full">
+                <div className="inner-index flex flex-col items-center justify-center">
+                    <div className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32">
+                        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+                                <div className="max-w-xl lg:max-w-lg">
+                                    <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">BMI healthy weight calculator</h2>
+                                    <p className="mt-4 text-lg leading-8 text-gray-300">
+                                        The body mass index (BMI) is a measure that uses your height and weight to work out if your weight is healthy.
+                                        This calculator for adult.
+                                    </p>
+                                    <div className="mt-6 flex max-w-md gap-x-4">
+                                        <label htmlFor="email-address" className="sr-only">
+                                            Email address
+                                        </label>
+                                        <form className="w-full max-w-lg">
+                                            <div className="flex flex-wrap -mx-3 mb-6">
+                                                <div className="w-full md:w-1/2 px-3  pt-2">
+                                                    <label 
+                                                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" 
+                                                    for="grid-last-name">
+                                                        Height
+                                                    </label>
+                                                    <input 
+                                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                                    id="grid-height" 
+                                                    type="number" placeholder="Height" />
+                                                </div>
+                                                <div className="w-full md:w-1/2 px-3 pt-2">
+                                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                                        Weight
+                                                    </label>
+                                                    <input 
+                                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                                    id="grid-weight" 
+                                                    type="number" placeholder="Weight" />
+                                                </div>
+                                                <div className="w-full md:w-1/2 px-3 pt-2">
+                                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                                        Age
+                                                    </label>
+                                                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                                    id="grid-age" type="number" placeholder="Age" />
+                                                </div>
+                                                <div className="w-full md:w-1/2 px-3 pt-2">
+                                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                                        Sex
+                                                    </label>
+                                                    <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-500 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                                    id="grid-sex">
+                                                        <option>Men</option>
+                                                        <option>Women</option>
+                                                    </select>
+                                                </div>
+                                            </div>
 
+                                            <button
+                                                type="submit"
+                                                className="flex-none w-full  pt-2 rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500"
+                                            >
+                                                Calculate
+                                            </button>
+                                        </form>
+
+                                    </div>
+                                </div>
+                                <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                                    <div className="flex flex-col items-start">
+
+                                        <dt className="mt-4 font-semibold text-white">How the BMI is calculated</dt>
+                                        <dd className="mt-2 leading-7 text-gray-400">
+                                            A BMI calculation in the healthy weight range is between 18.5 to 24.9.
+                                            The BMI calculation divides an adult's weight in kilograms (kg) by their height in metres (m) squared.
+
+                                        </dd>
+                                    </div>
+                                    <div className="flex flex-col items-start">
+
+                                        <dt className="mt-4 font-semibold text-white">Muscle mass</dt>
+                                        <dd className="mt-2 leading-7 text-gray-400">
+                                            The BMI calculation does not include muscle mass, which weighs more than fat.
+
+                                            This means muscular adults and athletes may be classed as overweight or obese even though their body fat is low.
+                                        </dd>
+                                    </div>
+                                </dl>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="cards w-full flex items-center justify-center">
+            <div id='price' className="cards w-full flex items-center justify-center">
                 <div className="py-24 sm:py-32">
                     <div className="mx-auto max-w-7xl px-6 lg:px-8 flex flex-col items-center justify-center">
                         <div className="mx-auto  lg:mx-0 flex flex-col items-center justify-center">
