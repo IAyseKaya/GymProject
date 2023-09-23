17e193a4b4dca1e55cba80de8457d5b5472a2cbf
IAyseKaya <i.ayse-kaya@hotmail.com>
2023-09-24 00:32:34 +0300

    appointment modal düzenlendi.

--- gym-project/src/components/appointment.jsx
+++ gym-project/src/components/appointment.jsx
@@ -4,7 +4,6 @@ import lessons from "../data/lessons";
 import { DayPicker, Row } from 'react-day-picker';
 import React,{ Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import { differenceInCalendarDays } from 'date-fns';
 import { format } from 'date-fns';
 import 'react-day-picker/dist/style.css';
@@ -18,13 +17,21 @@ function Appointment() {
     const[lesson,setLesson] = useState("");
     const[hour,setHour] = useState("");
     const[city,setCity] = useState("");
-    const[date,setDate] = useState("");
     
+    const[modal,setModal]= useState("");
 
     const modalMessage = (event) => {
         event.preventDefault();
-        console.log(name +" "+lastName+" "+email+" "+coach+" "+lesson+" "+hour+" "+city+" "+date);
-        setOpen(open);
+        setModal(
+        " Name Surname : "+name +" "+lastName+" , "+
+        " E-mail : "+email+" , "+
+        " Phone : "+phone+" , "+
+        " Coach : "+coach+" , "+
+        " Lesson : "+lesson+" , "+
+        " Hours : "+hour+" , "+
+        " City : "+city+" , "+
+        " Date : "+format(selected, 'PP'));
+        setOpen(true);
     }
     
 
@@ -44,7 +51,7 @@ function Appointment() {
         if (isPastRow) return <></>;
         return <Row {...props} />;
     }
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
 
     const cancelButtonRef = useRef(null)
     return (
@@ -60,10 +67,11 @@ function Appointment() {
                         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                             <div className="sm:col-span-3">
                                 <label htmlFor="first-name" className="block text-sm font-medium leading-6 ">
-                                    First name
+                                    First name 
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                    required
                                         type="text"
                                         name="first-name"
                                         id="first-name"
@@ -80,6 +88,7 @@ function Appointment() {
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                    required
                                         type="text"
                                         name="last-name"
                                         id="last-name"
@@ -111,6 +120,7 @@ function Appointment() {
                                 </label>
                                 <div className="mt-2">
                                     <input
+                                    required
                                         type="number"
                                         name="number"
                                         id="number"
@@ -126,6 +136,7 @@ function Appointment() {
                                 </label>
                                 <div className="mt-2">
                                     <select
+                                    required
                                         id="coaches"
                                         name="coaches"
                                         autoComplete="coaches-name"
@@ -145,6 +156,7 @@ function Appointment() {
                                 </label>
                                 <div className="mt-2">
                                     <select
+                                    required
                                         id="lessons"
                                         name="lessons"
                                         autoComplete="lessons-name"
@@ -165,19 +177,20 @@ function Appointment() {
                                 </label>
                                 <div className="mt-2">
                                     <select
+                                    required
                                         id="date"
                                         name="date"
                                         autoComplete="date"
                                         onChange={(e) => { setHour(e.target.value) }}
                                         className="block w-full rounded-md text-black  border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
                                     >
-                                        <option value="">09:00 - 10:00</option>
-                                        <option value="">10:00 - 11:00</option>
-                                        <option value="">11:00 - 12:00</option>
-                                        <option value="">14:00 - 15:00</option>
-                                        <option value="">15:00 - 16:00</option>
-                                        <option value="">16:00 - 17:00</option>
-                                        <option value="">17:00 - 18:00</option>
+                                        <option value="09:00 - 10:00">09:00 - 10:00</option>
+                                        <option value="10:00 - 11:00">10:00 - 11:00</option>
+                                        <option value="11:00 - 12:00">11:00 - 12:00</option>
+                                        <option value="14:00 - 15:00">14:00 - 15:00</option>
+                                        <option value="15:00 - 16:00">15:00 - 16:00</option>
+                                        <option value="16:00 - 17:00">16:00 - 17:00</option>
+                                        <option value="17:00 - 18:00">17:00 - 18:00</option>
                                     </select>
                                 </div>
                             </div>
@@ -187,14 +200,19 @@ function Appointment() {
                                     City
                                 </label>
                                 <div className="mt-2">
-                                    <input
-                                        type="text"
+                                <select
+                                required
                                         name="city"
                                         id="city"
                                         autoComplete="address-level2"
                                         onChange={(e) => { setCity(e.target.value) }}
                                         className="block w-full rounded-md text-black  border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                                    />
+                                    >
+                                        <option value="İstanbul">İstanbul</option>
+                                        <option value="Ankara">Ankara</option>
+                                        <option value="İzmir">İzmir</option>
+                                    </select>
+                                    
                                 </div>
                             </div>
                             <div className="sm:col-span-3 lg:col-span-2">
@@ -204,6 +222,7 @@ function Appointment() {
                                 <div className="mt-2 flex bg-white  items-center justify-center rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300">
 
                                     <DayPicker
+                                    required
                                         className="block  text-black  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
                                         mode="single"
                                         selected={selected}
@@ -212,66 +231,10 @@ function Appointment() {
                                         fromDate={new Date()}
                                         components={{ Row: OnlyFutureRow }}
                                         hidden={isPastDate}
-                                        onChange={(e) => { setDate(selected) }}
                                         showOutsideDays
                                     />
 
                                 </div>
-                            </div>
-                            <div className="lg:col-span-1"></div>
-                            <div className="sm:col-span-3 lg:col-span-3">
-                                <fieldset>
-                                    <legend className="text-sm font-semibold leading-6 ">Select Reminder Tool</legend>
-                                    <p className="text-gray-500">You can choose more than one reminder tool.</p>
-                                    <div className="mt-6 space-y-6">
-                                        <div className="relative flex gap-x-3">
-                                            <div className="flex h-6 items-center">
-                                                <input
-                                                    id="comments"
-                                                    name="comments"
-                                                    type="checkbox"
-                                                    className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-600"
-                                                />
-                                            </div>
-                                            <div className="text-sm leading-6">
-                                                <label htmlFor="comments" className="font-medium ">
-                                                    E-Mail
-                                                </label>
-
-                                            </div>
-                                        </div>
-                                        <div className="relative flex gap-x-3">
-                                            <div className="flex h-6 items-center">
-                                                <input
-                                                    id="candidates"
-                                                    name="candidates"
-                                                    type="checkbox"
-                                                    className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-600"
-                                                />
-                                            </div>
-                                            <div className="text-sm leading-6">
-                                                <label htmlFor="candidates" className="font-medium ">
-                                                    Send Text
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div className="relative flex gap-x-3">
-                                            <div className="flex h-6 items-center">
-                                                <input
-                                                    id="offers"
-                                                    name="offers"
-                                                    type="checkbox"
-                                                    className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-600"
-                                                />
-                                            </div>
-                                            <div className="text-sm leading-6">
-                                                <label htmlFor="offers" className="font-medium ">
-                                                    Call
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </fieldset>
                             </div>
                         </div>
                     </div>
@@ -290,7 +253,9 @@ function Appointment() {
                     >
                         Save
                     </button>
-                    <Transition.Root show={open} as={Fragment}>
+                    
+                </div>
+            </form><Transition.Root show={open} as={Fragment}>
                         <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
                             <Transition.Child
                                 as={Fragment}
@@ -318,16 +283,13 @@ function Appointment() {
                                         <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                                             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                                                 <div className="sm:flex sm:items-start">
-                                                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                                        <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                                                    </div>
                                                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                                         <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
                                                         Signed For The Course
                                                         </Dialog.Title>
                                                         <div className="mt-2">
                                                             <p className="text-sm text-gray-500">
-                                                               
+                                                                {modal}
                                                             </p>
                                                         </div>
                                                     </div>
@@ -337,13 +299,6 @@ function Appointment() {
                                                 <button
                                                     type="button"
                                                     className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                                                    onClick={() => setOpen(false)}
-                                                >
-                                                    Deactivate
-                                                </button>
-                                                <button
-                                                    type="button"
-                                                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                                                     onClick={() => setOpen(false)}
                                                     ref={cancelButtonRef}
                                                 >
@@ -356,8 +311,6 @@ function Appointment() {
                             </div>
                         </Dialog>
                     </Transition.Root>
-                </div>
-            </form>
         </div>
     );
 }
