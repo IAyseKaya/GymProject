8da92e7ef7495ba7937c76d4b29a2506163b79ea
IAyseKaya <i.ayse-kaya@hotmail.com>
2023-10-02 23:02:03 +0300

    text color added

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -28,7 +28,7 @@ return(
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5  shadow-sm 
+                  className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm 
                   ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -52,7 +52,7 @@ return(
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                  className="block text-black w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
